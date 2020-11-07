856854ad32cb09afc4e743c446b26c7708dc1d3e
naveennvrgup <naveennvrgup@gmail.com>
2020-11-07 19:29:32 +0530

    path queries

---
+++ Tree Algorithms/Path-Queries/main.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int,int> pii;
+
+const ll mx = 1e9 + 7;
+
+const int s=2e5+5;
+int n,q;
+vector<ll> arr, psum(s), val(s), start(s), finish(s);
+vector<vector<ll>> adj(s);
+
+class ST{
+    vector<ll> t;
+    int n;
+public:
+    ST(vector<ll> arr){
+        n=arr.size();
+        t.assign(n*4,0);
+    }
+
+
+    ll sum(int i, int tl, int tr, int pos){
+        if(tl==tr)return t[i];
+        else {
+            int tm=(tl+tr)/2;
+            if(pos<=tm)return t[i]+sum(i*2,tl,tm,pos);
+            else return t[i]+sum(i*2+1,tm+1,tr,pos);
+        }
+    }
+
+    void update(int i, int tl, int tr, int l, int r, ll val){
+        if(l>r)return;
+
+        if(l==tl && tr==r)t[i]+=val;
+        else{
+            int tm=(tl+tr)/2;
+            update(i*2,tl,tm,l,min(r,tm),val);
+            update(i*2+1,tm+1,tr,max(l,tm+1),r,val);
+        }
+    }
+};
+
+void dfs(int node, int parent, ll sum){
+    static int counter=-1;
+    
+    psum[node]=sum+val[node];
+    start[node]=++counter;
+
+    for(auto child: adj[node]){
+        if(child==parent)continue;
+        dfs(child,node, psum[node]);
+    }
+
+    finish[node]=counter;
+}
+
+
+void solve() {
+    cin>>n>>q;
+
+    for(int i=1;i<=n;i++)cin>>val[i];
+    for(int i=2;i<=n;i++){
+        ll a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    dfs(1,0,0);
+    arr.assign(n,0);
+    auto st=new ST(arr);
+
+    for(int i=0;i<q;i++){
+        ll type;
+        cin>>type;
+        if(type==1){
+            ll i,x;
+            cin>>i>>x;
+            st->update(1,0,n-1,start[i],finish[i],x-val[i]);
+        }else{
+            ll i;
+            cin>>i;
+            cout<<st->sum(1,0,n-1,start[i])+psum[i]<<endl;
+        }
+    }
+    
+}
+
+// --------------------------------
+// shows WA for half the TC
+// --------------------------------
+int main() {
+    FASTIO
+    solve();
+
+//    int t;
+//    cin>>t;
+//    while(t--)solve();
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ const ll mx = 1e9 + 7;
 
 const int s=2e5+5;
 int n,q;
-vector<ll> arr, val(s), start(s), finish(s);
+vector<ll> arr, psum(s), val(s), start(s), finish(s);
 vector<vector<ll>> adj(s);
 
 class ST{
@@ -20,52 +20,42 @@ public:
     ST(vector<ll> arr){
         n=arr.size();
         t.assign(n*4,0);
-        build(1,0,n-1);
     }
 
-    void build(int i, int tl, int tr){
-        if(tl==tr)t[i]=arr[tl];
-        else{
-            int tm=(tl+tr)/2;
-            build(i*2,tl,tm);
-            build(i*2+1,tm+1,tr);
-            t[i]=t[i*2]+t[i*2+1];
-        }
-    }
 
-    ll sum(int i, int tl, int tr, int l, int r){
-        if(l>r)return 0;
-
-        if(tl==l && tr==r)return t[i];
+    ll sum(int i, int tl, int tr, int pos){
+        if(tl==tr)return t[i];
         else {
             int tm=(tl+tr)/2;
-            return sum(i*2,tl,tm,l,min(r,tm))+sum(i*2+1,tm+1,tr,max(l,tm+1),r);
+            if(pos<=tm)return t[i]+sum(i*2,tl,tm,pos);
+            else return t[i]+sum(i*2+1,tm+1,tr,pos);
         }
     }
 
-    void update(int i, int tl, int tr, int pos, int val){
-        if(tl==tr)t[i]=val;
+    void update(int i, int tl, int tr, int l, int r, ll val){
+        if(l>r)return;
+
+        if(l==tl && tr==r)t[i]+=val;
         else{
             int tm=(tl+tr)/2;
-            if(pos<=tm)update(i*2,tl,tm,pos,val);
-            else update(i*2+1,tm+1,tr,pos,val);
-            t[i]=t[i*2]+t[i*2+1];
+            update(i*2,tl,tm,l,min(r,tm),val);
+            update(i*2+1,tm+1,tr,max(l,tm+1),r,val);
         }
     }
 };
 
-void dfs(int node, int parent){
+void dfs(int node, int parent, ll sum){
     static int counter=-1;
-
+    
+    psum[node]=sum+val[node];
     start[node]=++counter;
-    arr.push_back(val[node]);
 
     for(auto child: adj[node]){
         if(child==parent)continue;
-        dfs(child,node);
+        dfs(child,node, psum[node]);
     }
 
-    finish[node]=counter+1;
+    finish[node]=counter;
 }
 
 
@@ -80,7 +70,8 @@ void solve() {
         adj[b].push_back(a);
     }
 
-    dfs(1,0);
+    dfs(1,0,0);
+    arr.assign(n,0);
     auto st=new ST(arr);
 
     for(int i=0;i<q;i++){
@@ -89,11 +80,11 @@ void solve() {
         if(type==1){
             ll i,x;
             cin>>i>>x;
-            st->update(1,0,n-1,start[i],x);
+            st->update(1,0,n-1,start[i],finish[i],x-val[i]);
         }else{
             ll i;
             cin>>i;
-            cout<<st->sum(1,0,n-1,start[i],n-1)-st->sum(1,0,n-1,finish[i],n-1)<<endl;
+            cout<<st->sum(1,0,n-1,start[i])+psum[i]<<endl;
         }
     }
     
